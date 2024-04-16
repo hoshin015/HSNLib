ba3bcfa6a6c92a840365b8ec28a6fb16455351c3
unknown0081 <hayataku0810@gmail.com>
2024-04-16 12:33:44 +0900

    テストシーン作成

---
+++ SpinningTopPlayerManager.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <vector>
+#include <set>
+#include "StPlayer.h"
+
+class SpinningTopPlayerManager {
+private:
+	SpinningTopPlayerManager() {}
+	~SpinningTopPlayerManager() {}
+
+public:
+	// 唯一のインスタンス取得
+	static SpinningTopPlayerManager& Instance() {
+		static SpinningTopPlayerManager instance;
+		return instance;
+	}
+
+	// 更新処理
+	void Update();
+
+	// 描画処理
+	void Render();
+
+	// プレイヤー登録
+	void Register(StPlayer* player);
+
+	// プレイヤー削除
+	void Remove(StPlayer* player);
+
+	// プレイヤー全削除
+	void Clear();
+
+	// プレイヤー数取得
+	int GetPlayerCount() const { return static_cast<int>(players.size()); }
+
+	// プレイヤー取得
+	StPlayer* GetPlayer(int index) { return players.at(index); }
+
+	// デバッグ用GUI描画
+	void DrawDebugGui();
+
+private:
+	std::vector<StPlayer*> players;
+	std::set<StPlayer*> removes;
+};
+
