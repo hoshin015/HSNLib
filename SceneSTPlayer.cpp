b5c075d737b1c1b189f4b26d8eab60ff566d4505
unknown0081 <hayataku0810@gmail.com>
2024-04-17 12:30:58 +0900

    move

---
+++ SceneSTPlayer.cpp
@@ -0,0 +1,175 @@
+#include <tchar.h>
+#include "SceneSTPlayer.h"
+#include "SceneManager.h"
+#include "Library/Input/InputManager.h"
+#include "Library/ImGui/Include/imgui.h"
+#include "Library/Timer.h"
+#include "StageManager.h"
+#include "StageContext.h"
+#include "LightManager.h"
+#include "SpinningTopEnemyManager.h"
+#include "StEnemy01.h"
+
+#include "SpinningTopPlayerManager.h"
+#include "StPlayer.h"
+
+void SceneSTPlayer::Initialize() {
+	StEnemy01* slime = new_ StEnemy01();
+	slime->SetPosition({ 0, 0, 0 });
+	SpinningTopEnemyManager::Instance().Register(slime);
+
+	StPlayer* player = new_ StPlayer();
+	SpinningTopPlayerManager::Instance().Register(player);
+
+	// ステージ初期化
+	StageManager& stageManager = StageManager::Instance();
+	StageContext* stageMain = new_ StageContext();
+	stageManager.Register(stageMain);
+
+	// ライト初期化
+	Light* directionLight = new Light(LightType::Directional);
+	directionLight->SetDirection(DirectX::XMFLOAT3(0.5, -1, -1));
+	directionLight->SetColor(DirectX::XMFLOAT4(1, 1, 1, 1));
+	LightManager::Instance().Register(directionLight);
+
+	// カメラ初期設定
+	Camera::Instance().SetLookAt(
+		DirectX::XMFLOAT3(0, 20, 40),		// カメラ座標
+		DirectX::XMFLOAT3(-90, 0, -30),		// ターゲット(設定しても意味ない)
+		DirectX::XMFLOAT3(0, 1, 0)			// 上方向ベクトル
+	);
+	Camera::Instance().SetAngle({ DirectX::XMConvertToRadians(30), DirectX::XMConvertToRadians(180), 0 });
+
+	Camera::Instance().cameraType = Camera::CAMERA::FREE;
+}
+
+void SceneSTPlayer::Finalize() {
+	StageManager::Instance().Clear();
+
+	LightManager::Instance().Clear();
+
+	SpinningTopEnemyManager::Instance().Clear();
+
+	SpinningTopPlayerManager::Instance().Clear();
+}
+
+void SceneSTPlayer::Update() {
+	// カメラコントローラー更新処理
+	Camera::Instance().Update();
+
+
+	InputManager& input = InputManager::Instance();
+
+	if (input.GetKeyPressed(Keyboard::Enter)) {
+		SceneManager::Instance().ChangeScene(new SceneLoading(new SceneGame));
+	}
+
+	StageManager::Instance().Update();
+
+	SpinningTopEnemyManager::Instance().Update();
+	SpinningTopPlayerManager::Instance().Update();
+}
+
+void SceneSTPlayer::Render() {
+	// --- Graphics 取得 ---
+	Graphics& gfx = Graphics::Instance();
+
+	// shadowMap
+	{
+		gfx.SetDepthStencil(DEPTHSTENCIL_STATE::ZT_ON_ZW_ON);
+
+		gfx.shadowBuffer->Clear();
+		gfx.shadowBuffer->Activate();
+
+
+		// カメラパラメータ設定
+		{
+			// 平行光源からカメラ位置を作成し、そこから原点の位置を見るように視線行列を生成
+			DirectX::XMFLOAT3 dir = LightManager::Instance().GetLight(0)->GetDirection();
+			DirectX::XMVECTOR LightPosition = DirectX::XMLoadFloat3(&dir);
+			LightPosition = DirectX::XMVectorScale(LightPosition, -250.0f);
+			DirectX::XMMATRIX V = DirectX::XMMatrixLookAtLH(LightPosition,
+				DirectX::XMVectorSet(0.0f, 0.0f, 0.0f, 0.0f),
+				DirectX::XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f));
+
+			// シャドウマップに描画したい範囲の射影行列を生成
+			DirectX::XMMATRIX P = DirectX::XMMatrixOrthographicLH(gfx.shadowDrawRect, gfx.shadowDrawRect, 0.1f, 1000.0f);
+			XMMATRIX viewProjection = V * P;
+			DirectX::XMStoreFloat4x4(&gfx.shadowMapData.lightViewProjection, viewProjection);	// ビュー　プロジェクション　変換行列をまとめる
+		}
+
+
+		gfx.deviceContext->UpdateSubresource(gfx.constantBuffers[5].Get(), 0, 0, &gfx.shadowMapData, 0, 0);
+		gfx.deviceContext->VSSetConstantBuffers(3, 1, gfx.constantBuffers[5].GetAddressOf());
+
+		gfx.deviceContext->VSSetShader(gfx.vertexShaders[static_cast<size_t>(VS_TYPE::ShadowMapCaster_VS)].Get(), nullptr, 0);
+		gfx.deviceContext->PSSetShader(nullptr, nullptr, 0);
+
+		StageManager::Instance().Render();
+
+		SpinningTopEnemyManager::Instance().Render();
+		SpinningTopPlayerManager::Instance().Render();
+
+		gfx.shadowBuffer->DeActivate();
+	}
+
+
+	// --- ImGuiのデバッグ適用処理 ---
+	gfx.SetRasterizer(static_cast<RASTERIZER_STATE>(RASTERIZER_STATE::CLOCK_FALSE_SOLID));
+
+	Graphics::SceneConstants data {};
+	XMMATRIX viewProjection = XMLoadFloat4x4(&Camera::Instance().GetView()) * XMLoadFloat4x4(&Camera::Instance().GetProjection());
+	DirectX::XMStoreFloat4x4(&data.viewProjection, viewProjection);	// ビュー　プロジェクション　変換行列をまとめる
+
+	LightManager::Instance().PushLightData(data);
+
+	data.cameraPosition = { Camera::Instance().GetEye().x, Camera::Instance().GetEye().y, Camera::Instance().GetEye().z, 0 };
+	gfx.deviceContext->UpdateSubresource(gfx.constantBuffer.Get(), 0, 0, &data, 0, 0);
+	gfx.deviceContext->VSSetConstantBuffers(1, 1, gfx.constantBuffer.GetAddressOf());
+	gfx.deviceContext->PSSetConstantBuffers(1, 1, gfx.constantBuffer.GetAddressOf());
+
+	// shadowMap のバインド
+	gfx.deviceContext->PSSetShaderResources(4, 1, gfx.shadowBuffer->shaderResourceView.GetAddressOf());
+	gfx.deviceContext->PSSetSamplers(3, 1, gfx.samplerStates[static_cast<size_t>(SAMPLER_STATE::SHADOWMAP)].GetAddressOf());
+
+	gfx.deviceContext->VSSetShader(gfx.vertexShaders[static_cast<size_t>(VS_TYPE::SkinnedMesh_VS)].Get(), nullptr, 0);
+	gfx.deviceContext->PSSetShader(gfx.pixelShaders[static_cast<size_t>(PS_TYPE::SkinnedMesh_PS)].Get(), nullptr, 0);
+
+	gfx.deviceContext->UpdateSubresource(gfx.constantBuffers[5].Get(), 0, 0, &gfx.shadowMapData, 0, 0);
+	gfx.deviceContext->VSSetConstantBuffers(3, 1, gfx.constantBuffers[5].GetAddressOf());
+	gfx.deviceContext->PSSetConstantBuffers(3, 1, gfx.constantBuffers[5].GetAddressOf());
+
+	StageManager::Instance().Render();
+
+	SpinningTopEnemyManager::Instance().Render();
+	SpinningTopPlayerManager::Instance().Render();
+
+	// --- デバッグ描画 ---
+	DrawDebugGUI();
+}
+
+void SceneSTPlayer::DrawDebugGUI() {
+	if (ImGui::BeginMainMenuBar()) {
+		if (ImGui::BeginMenu("File")) {
+			if (ImGui::BeginMenu("Scene")) {
+				if (ImGui::MenuItem("Title")) {
+					SceneManager::Instance().ChangeScene(new SceneTitle);
+				}
+				if (ImGui::MenuItem("Game")) {
+					SceneManager::Instance().ChangeScene(new SceneLoading(new SceneGame));
+				}
+				if (ImGui::MenuItem("Animation")) {
+					SceneManager::Instance().ChangeScene(new SceneAnimation);
+				}
+				if (ImGui::MenuItem("ContextBase")) {
+					SceneManager::Instance().ChangeScene(new SceneLoading(new SceneContextBase));
+				}
+				ImGui::EndMenu();
+			}
+			ImGui::EndMenu();
+		}
+		ImGui::EndMainMenuBar();
+	}
+
+	SpinningTopPlayerManager::Instance().DrawDebugGui();
+}
--- SceneSTPlayer.h
+++ SceneSTPlayer.h
@@ -1,1 +1,28 @@
 #pragma once
+#include <memory>
+#include "Scene.h"
+#include "Library/3D/SkinnedMesh.h"
+
+// テストシーン
+class SceneSTPlayer : public Scene {
+public:
+	SceneSTPlayer() {}
+	~SceneSTPlayer() override {};
+
+	// 初期化
+	void Initialize() override;
+
+	// 終了化
+	void Finalize() override;
+
+	// 更新
+	void Update() override;
+
+	// 描画
+	void Render() override;
+
+	// デバッグ描画
+	void DrawDebugGUI();
+
+private:
+};
