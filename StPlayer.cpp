7291892bd6d9e6ab82c53bc9e94cd95599313d66
Unknown <hayataku0810@gmail.com>
2024-04-18 12:30:39 +0900

    Update move 作成中

--- StPlayer.cpp
+++ StPlayer.cpp
@@ -3,10 +3,16 @@
 #include "Library/3D/ResourceManager.h"
 #include "Library/Input/InputManager.h"
 #include "Library/ImGui/Include/imgui.h"
+#include "Library/Timer.h"
 using namespace DirectX;
 
 StPlayer::StPlayer() {
 	model = ResourceManager::Instance().LoadModelResource("Data/Fbx/Jummo/Jummo.fbx");
+
+	maxSpeed = 10;
+	rotateSpeed = 2;
+	speed = 0;
+	moveDirection = { 0,0 };
 }
 
 StPlayer::~StPlayer() {}
@@ -14,7 +20,8 @@ StPlayer::~StPlayer() {}
 void StPlayer::Update() {
 	Input();
 
-	angle.y+=2;
+	angle.y += 2;
+	UpdateMove();
 
 	// 速力更新処理
 	UpdateVelocity();
@@ -33,20 +40,39 @@ void StPlayer::Render() {
 
 void StPlayer::DrawDebugGui() {
 	if (ImGui::Begin("Player")) {
-		ImGui::Text("aaaa");
-		if (ImGui::BeginListBox("Input")) {
+		ImGui::DragFloat("Speed", &maxSpeed, 0.1f);
+
+		if (ImGui::CollapsingHeader(u8"インプット", ImGuiTreeNodeFlags_DefaultOpen)) {
 			XMFLOAT2 outDebugFloat2;
 			float outDebugFloat;
 			int outDebugInt;
 
-			outDebugFloat2 = std::get<XMFLOAT2>(inputAnalogMap["Move"]);
+			outDebugFloat2 = GetInputMap<XMFLOAT2>("Move");
 			ImGui::Text("Move:%.3f,%.3f", outDebugFloat2.x, outDebugFloat2.y);
 
-			outDebugInt = inputDigitalMap["Attack"];
+			outDebugInt = GetInputMap<bool>("Attack");
 			ImGui::Text("Attack:%d", outDebugInt);
 
-			ImGui::EndListBox();
 		}
+		if (ImGui::CollapsingHeader(u8"デバック", ImGuiTreeNodeFlags_DefaultOpen)) {
+			XMFLOAT2 outDebugFloat2;
+			float outDebugFloat;
+			int outDebugInt;
+
+			outDebugFloat2 = { GetDebugValue<float>("x"),GetDebugValue<float>("y") };
+			ImGui::Text("debug:%.3f,%.3f", outDebugFloat2.x, outDebugFloat2.y);
+
+			//outDebugFloat = GetDebugValue<float>("t");
+			//ImGui::Text("t:%.3f", outDebugFloat);
+
+			outDebugFloat = GetDebugValue<float>("direction");
+			ImGui::Text("direction:%.3f", outDebugFloat);
+
+			ImGui::Text("speed:%.3f", speed);
+			ImGui::Text("frame:%.3f", 0.99999999f * Timer::Instance().DeltaTime());
+
+		}
+
 		ImGui::End();
 	}
 }
@@ -64,10 +90,30 @@ void StPlayer::Input() {
 		movefloat2.y = im.GetThumSticksLeftY();
 	}
 
-	inputAnalogMap["Move"] = movefloat2;
+	inputMap["Move"] = movefloat2;
 
-	inputDigitalMap["Attack"] = im.GetKeyPressed(Keyboard::Space) | im.GetGamePadButtonPressed(GAMEPADBUTTON_STATE::a);
-	Move(movefloat2.x, movefloat2.y, 10);
+	bool attack = im.GetKeyPress(Keyboard::Space) || im.GetGamePadButtonPress(GAMEPADBUTTON_STATE::a);
+	inputMap["Attack"] = attack;
+}
+
+void StPlayer::UpdateMove() {
+	XMFLOAT2 input = GetInputMap<XMFLOAT2>("Move");
+	XMVECTOR inputVec = XMLoadFloat2(&input);
+	XMVECTOR direction = XMLoadFloat2(&moveDirection);
+
+	speed -= speed * 0.98f * Timer::Instance().DeltaTime();
+	//inputVec *= speed;
+	if (fabsf(input.x) > 0.00001f || fabsf(input.y) > 0.00001f) {
+		speed += XMVectorGetX(XMVector2Length(inputVec)) * Timer::Instance().DeltaTime();
+	}
+
+	XMStoreFloat2(&moveDirection, inputVec * speed);
+
+	debugValue["x"] = moveDirection.x;
+	debugValue["y"] = moveDirection.y;
+	debugValue["direction"] = XMVectorGetX(XMVector2Length(XMLoadFloat2(&moveDirection)));
+
+	Move(moveDirection.x, moveDirection.y, maxSpeed);
 }
 
 void StPlayer::OnLanding() {}
