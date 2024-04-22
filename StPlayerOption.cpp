57da624572d06909eab981aefe81f37d2d07fe49
Unknown <hayataku0810@gmail.com>
2024-04-22 23:34:51 +0900

    子機クラス追加

---
+++ StPlayerOption.cpp
@@ -0,0 +1,24 @@
+#include "StPlayerOption.h"
+#include "Library/3D/DebugPrimitive.h"
+#include "Library/3D/ResourceManager.h"
+#include "Library/Input/InputManager.h"
+#include "Library/ImGui/Include/imgui.h"
+#include "Library/Timer.h"
+#include "Library/3D/Camera.h"
+
+#include "Collision.h"
+
+StPlayerOption::StPlayerOption() {
+	model = ResourceManager::Instance().LoadModelResource("Data/Fbx/SpinningTopTest/SpinningTopTest.fbx");
+
+}
+
+StPlayerOption::~StPlayerOption() {}
+
+void StPlayerOption::Update() {
+
+}
+
+void StPlayerOption::Render() {}
+
+void StPlayerOption::DrawDebugGui() {}
